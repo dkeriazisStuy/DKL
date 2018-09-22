b8345c63284e992ccf24139716655ba668cc89de
dkeriazisStuy <dkeriazis@stuy.edu>
2018-09-22 12:04:35 -0400

    Moved csv to data

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
 from csv import reader
-from occupation_handling import csv_to_weighted_list, weighted_choice, read_file  # Custom import
+from occupation_handling import csv_to_weighted_list, weighted_choice, read_file
 
 app = Flask(__name__)
 
-# Helper
-def csv_to_list():
-    with open('occupations.csv') as f:
-      return list(reader(f))
+# Returns the contents of `csv_file` as a list of lists
+def csv_to_list(csv_file):
+    with open(csv_file) as f:
+        return list(reader(f))
 
 @app.route('/occupations')
 def occupations():
-  rows = csv_to_list()[1:-1] # Strip out first and last lines
-  occupations = read_file('occupations.csv')
-  weighted_list = csv_to_weighted_list(occupations)
-  job = weighted_choice(weighted_list)
-  return render_template('occupation_template.html', rows=rows, job=job)
+    csv_file = 'data/occupations.csv'
+    # Strip out first and last lines
+    rows = csv_to_list(csv_file)[1:-1]
+    occupations = read_file(csv_file)
+    weighted_list = csv_to_weighted_list(occupations)
+    job = weighted_choice(weighted_list)
+    return render_template('occupation_template.html', rows=rows, job=job)
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+    app.debug = True
+    app.run()
 
