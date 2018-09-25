252bebfe47101c58d22b8ce04bd50a289d2d39b0
dkeriazis <dkeriazis@gmail.com>
2018-09-25 13:17:02 -0400

    Formatting updates

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -5,6 +5,8 @@
 
 from flask import Flask, render_template
 from csv import reader
+# Import handling for converting csv to a weighted list 
+# and choosing a random occupation by weight
 from util.occupation_handling import csv_to_weighted_list, \
         weighted_choice, read_file
 
@@ -20,8 +22,10 @@ def occupations():
     csv_file = 'data/occupations.csv'
     # Strip out first and last lines
     rows = csv_to_list(csv_file)[1:-1]
+    # Reads CSV file
     occupations = read_file(csv_file)
     weighted_list = csv_to_weighted_list(occupations)
+    # Get job by weight
     job = weighted_choice(weighted_list)
     return render_template('occupation_template.html', rows=rows, job=job)
 
