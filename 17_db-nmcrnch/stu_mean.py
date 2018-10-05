1fa4ad9cc3e3ddd8f1c6433016ae898d0ef15d3d
dkeriazis <dkeriazis@gmail.com>
2018-10-05 13:19:48 -0400

    Began HW 17

---
+++ 17_db-nmcrnch/stu_mean.py
@@ -0,0 +1,36 @@
+import sqlite3  # Enable control of an sqlite database
+import csv  # Facilitates CSV I/O
+
+DB_FILE="database.db"
+
+db = sqlite3.connect(DB_FILE)  # Open if file exists, otherwise create
+c = db.cursor()  # Facilitate db ops
+
+counts = {}
+totals = {}
+averages = {}
+names = {}
+
+command = "SELECT name,code,mark,courses.id FROM courses,peeps WHERE courses.id = peeps.id"
+c.execute(command)
+
+for i in c.fetchall():
+    name, code, mark, student = i
+    if student not in counts:
+        counts[student] = 0
+        totals[student] = 0
+    counts[student] += 1
+    totals[student] += mark
+    names[student] = name
+
+# print(totals, counts)
+
+for student in counts:
+    total = totals[student]
+    count = counts[student]
+    averages[student] = round(total / count, 1)
+    print(names[student], student, averages[student])
+
+db.commit()  # Save changes
+db.close()  # Close database
+
